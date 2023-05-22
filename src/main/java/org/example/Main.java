3ef59347614d1d508b6000bc803cc88cf7b35e0f
ruha <anbro2002@gmail.com>
2023-05-22 23:39:22 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,40 +9,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        SouvenirsCRUD crud = new SouvenirsCRUD();
-        Souvenir souvenir = new Souvenir("Vaza", "USZoloto", LocalDate.now(), 15000);
-        Souvenir souvenir1 = new Souvenir("Vaza1", "UkrZoloto", LocalDate.now(), 15000);
-        Souvenir souvenir4 = new Souvenir("Vaza4", "Zolotiy vik", LocalDate.now(), 150004);
-        Souvenir souvenir2 = new Souvenir("Vaza2", "IceBox", LocalDate.now(), 150002);
 
-        Manufacturer manufacturer = new Manufacturer("Zolotiy vik", "Ukraine");
-        Manufacturer manufacturer3 = new Manufacturer("UkrZoloto", "Ukraine");
-        Manufacturer manufacturer4 = new Manufacturer("USZoloto", "USA");
-        Manufacturer manufacturer1 = new Manufacturer("IceBox", "USA");
-
-        try {
-            crud.addSouvenir(souvenir);
-            crud.addSouvenir(souvenir2);
-            crud.addSouvenir(souvenir4);
-            crud.addSouvenir(souvenir1);
-
-            crud.addManufacturer(manufacturer);
-            crud.addManufacturer(manufacturer1);
-            crud.addManufacturer(manufacturer3);
-            crud.addManufacturer(manufacturer4);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (CsvRequiredFieldEmptyException e) {
-            throw new RuntimeException(e);
-        } catch (CsvDataTypeMismatchException e) {
-            throw new RuntimeException(e);
-        }
-
-        crud.readAllSouvenirs().forEach(System.out::println);
-        crud.readAllManufacturers().forEach(System.out::println);
-
-        crud.deleteManufacturer("IceBox");
-
-        crud.printAllInfo();
     }
 }
--- src/main/java/org/example/SouvenirsCRUD.java
+++ src/main/java/org/example/SouvenirsCRUD.java
@@ -10,9 +10,9 @@ import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.SQLOutput;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class SouvenirsCRUD {
@@ -23,12 +23,7 @@ public class SouvenirsCRUD {
     public SouvenirsCRUD() {}
 
 
-
-    public void changeSouvenir() {
-
-    }
-
-    public void addSouvenir(Souvenir souvenir) throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
+    public void addSouvenir(Souvenir souvenir) {
 
         try (Writer writer  = new FileWriter(souvenirsFile.toPath().toString(), true)) {
 
@@ -37,6 +32,12 @@ public class SouvenirsCRUD {
                     .build();
 
             sbc.write(souvenir);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CsvRequiredFieldEmptyException e) {
+            throw new RuntimeException(e);
+        } catch (CsvDataTypeMismatchException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -73,22 +74,38 @@ public class SouvenirsCRUD {
         }
     }
 
+    public List<Souvenir> readAllSouvenirs() {
+        try (FileReader reader = new FileReader(souvenirsFile)) {
+            CsvToBean<Souvenir> csvToBean = new CsvToBeanBuilder<Souvenir>(reader)
+                    .withType(Souvenir.class)
+                    .withSeparator(',')
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            return csvToBean.parse();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+//• Вивести інформацію про сувеніри заданого виробника.
     public List<Souvenir> getSouvenirsByManufacturer(String name) {
         List<Souvenir> list = readAllSouvenirs();
 
         return list.stream().filter(s -> s.getRequisites().equals(name)).toList();
     }
-
+//• Вивести інформацію про сувеніри, виготовлені в заданій країні.
     public List<Souvenir> getSouvenirsByCountry(String country) {
         List<String> mNames = readAllManufacturers().stream().filter(m -> m.getCountry().equals(country)).map(Manufacturer::getName).toList();
         return readAllSouvenirs().stream().filter(s -> mNames.contains(s.getRequisites())).toList();
     }
-
+//• Вивести інформацію про виробників, чиї ціни на сувеніри менше заданої.
     public List<Manufacturer> getBySouvenirsPriceLessThan(int price) {
         List<String> requisites = readAllSouvenirs().stream().filter(s -> s.getPrice() < price).map(Souvenir::getRequisites).toList();
         return readAllManufacturers().stream().filter(m -> requisites.contains(m.getName())).toList();
     }
-
+//• Вивести інформацію по всіх виробниках та, для кожного виробника вивести інформацію про всі сувеніри, які він виробляє.
     public void printAllInfo() {
         List<Manufacturer> manList = readAllManufacturers();
 
@@ -106,23 +123,32 @@ public class SouvenirsCRUD {
                                 + " Price: " + s.getPrice());
         }
     }
-
-    public List<Souvenir> getAllByYear(int year) {
-        return readAllSouvenirs().stream().filter(s -> s.getDate().getYear() == year).collect(Collectors.toList());
+// • Для кожного року вивести список сувенірів, зроблених цього року.
+    public void getAllByAllYears() {
+        Set<Integer> years = readAllSouvenirs().stream().map(Souvenir::getDate).map(LocalDate::getYear).collect(Collectors.toSet());
+        for (Integer y : years) {
+            System.out.println("Year " + y + ": ");
+            List<Souvenir> list = readAllSouvenirs().stream().filter(s -> s.getDate().getYear() == y).toList();
+            System.out.println(list);
+        }
     }
 
-
-    public void deleteManufacturer(String name) {
-        List<Manufacturer> list = readAllManufacturers();
+//  Видалити заданого виробника та його сувеніри.
+    public void deleteManufacturerAndSouvenirs(String name) {
+        List<Manufacturer> manufacturers = readAllManufacturers();
+        List<Souvenir> souvenirs = getSouvenirsByManufacturer(name);
         try {
             Files.delete(manufacturersFile.toPath());
+            Files.delete(souvenirsFile.toPath());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        list.stream().filter(m -> !m.getName().equals(name)).forEach(this::addManufacturer);
-    }
+        manufacturers.stream().filter(m -> !m.getName().equals(name)).forEach(this::addManufacturer);
+        souvenirs.stream().filter(s -> !s.getRequisites().equals(name)).forEach(this::addSouvenir);
 
+    }
+//• Вивести інформацію про виробників заданого сувеніру, виробленого у заданому року.
     public void printAllMansBySouvNameAndYear(String name, int year) {
         List<String> mNames = readAllSouvenirs().stream().filter(s -> s.getName().equals(name)
                 && (s.getDate().getYear() == year)).map(Souvenir::getRequisites).collect(Collectors.toList());
@@ -133,22 +159,4 @@ public class SouvenirsCRUD {
             System.out.println(man);
         }
     }
-
-
-    public List<Souvenir> readAllSouvenirs() {
-        try (FileReader reader = new FileReader(souvenirsFile)) {
-            CsvToBean<Souvenir> csvToBean = new CsvToBeanBuilder<Souvenir>(reader)
-                    .withType(Souvenir.class)
-                    .withSeparator(',')
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-
-            return csvToBean.parse();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
